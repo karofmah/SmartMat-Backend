f9d7c8b7ec103e645ae175a37993309840b47924
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-04-19 10:54:01 +0200

    Existing user may be retrieved from database

--- src/main/java/idatt2106v231/backend/model/User.java
+++ src/main/java/idatt2106v231/backend/model/User.java
@@ -1,4 +1,61 @@
 package idatt2106v231.backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    /**
+     * The email address of the user.
+     */
+    @Id
+    private String email;
+
+    /**
+     * password of the user.
+     */
+    @Column
+    private String password;
+
+    /**
+     * The first name of the user.
+     */
+    @Column
+    private String firstName;
+
+    /**
+     * The last name of the user.
+     */
+    @Column
+    private String lastName;
+
+    /**
+     * The phone number of the user.
+     */
+    @Column
+    private int phoneNumber;
+
+    /**
+     * The age of the user.
+     */
+    @Column
+    private int age;
+
+    /**
+     * The number of members in the household the user.
+     */
+    @Column
+    private int household;
+
 }
--- src/main/java/idatt2106v231/backend/service/UserServices.java
+++ src/main/java/idatt2106v231/backend/service/UserServices.java
@@ -1,4 +1,53 @@
 package idatt2106v231.backend.service;
 
+import idatt2106v231.backend.model.User;
+import idatt2106v231.backend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserServices {
+
+    private static final Logger _logger =
+            LoggerFactory.getLogger(UserServices.class);
+    private UserRepository userRepository;
+
+    /**
+     * Sets the user repository to use for database access.
+     *
+     * @param userRepository the user repository to use.
+     */
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    /**
+     * This method retrieves a user with a specified email address from the database.
+     *
+     * @param email the email address of the user to retrieve.
+     * @return an Optional object containing the user with the specified email address, or an empty Optional object if the user does not exist in the database.
+     */
+    public Optional<User> getUser(String email) {
+        Optional<User> userOptional = userRepository.findAll()
+                .stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst()
+                .or(Optional::empty);
+
+        if (userOptional.isPresent()) {
+            _logger.info("Successfully retrieved user with email: " + email);
+        } else {
+            _logger.info("Could not find user with email: " + email);
+        }
+
+        return userOptional;
+    }
+
+
 }
