c6fe191c9e7f2751a4cc4c3e5a83aaec1bd75066
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-04-19 16:06:15 +0200

    Added exception handling for retrieving user

--- src/main/java/idatt2106v231/backend/controller/UserController.java
+++ src/main/java/idatt2106v231/backend/controller/UserController.java
@@ -1,13 +1,12 @@
+
 package idatt2106v231.backend.controller;
 
-import idatt2106v231.backend.model.User;
+import idatt2106v231.backend.dto.user.UserCreationDto;
 import idatt2106v231.backend.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,12 +20,17 @@ public class UserController {
     }
 
     @GetMapping("/login/user")
-    public ResponseEntity<Optional<User>> getUser(@RequestParam String email){
+    public ResponseEntity<Object> getUser(@RequestParam String email){
         try{
-            Optional <User> user=userServices.getUser(email);
+            UserCreationDto user=userServices.getUser(email);
+
+            if(user==null){
+                return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+            }
+
             return new ResponseEntity<>(user, HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
     }
