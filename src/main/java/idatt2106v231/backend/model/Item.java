bf08c46ff8d99cb8f2441ec28171c97c1a8988e0
Anna Tvedt <amtvedt@stud.ntnu.no>
2023-04-20 08:56:56 +0200

    Implemented model classes for Refrigerator, Item and ItemCategory

---
+++ src/main/java/idatt2106v231/backend/model/Item.java
@@ -0,0 +1,52 @@
+package idatt2106v231.backend.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @Column
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int itemId;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "item_category_id", referencedColumnName = "itemCategoryId")
+    @NotNull
+    private ItemCategory category;
+
+    @OneToMany(mappedBy = "item")
+    private List<ItemRefrigerator> itemInRefrigerators = new ArrayList<>(); //??
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return name.equals(item.name) || itemId==item.getItemId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
