22abe0b99765cd921bc1f5c8a0a7e6a5755a52d9
Anna Tvedt <amtvedt@stud.ntnu.no>
2023-04-20 14:51:41 +0200

    Small changes to ItemRefrigeratorRepository and ItemRefrigeratorController

---
+++ src/main/java/idatt2106v231/backend/repository/ItemRefrigeratorRepository.java
@@ -0,0 +1,14 @@
+package idatt2106v231.backend.repository;
+
+import idatt2106v231.backend.model.ItemRefrigerator;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRefrigeratorRepository extends CrudRepository<ItemRefrigerator, Integer> {
+
+    Optional<ItemRefrigerator> findAllByItemItemId(int itemId);
+
+}
