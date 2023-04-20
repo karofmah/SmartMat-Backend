9ff117883b6256ae34c853b96bb6b733c4f098a1
Anna Tvedt <amtvedt@stud.ntnu.no>
2023-04-20 10:38:19 +0200

    Implemented services classes for Item, Category and Refrigerator

---
+++ src/main/java/idatt2106v231/backend/service/RefrigeratorServices.java
@@ -0,0 +1,58 @@
+package idatt2106v231.backend.service;
+
+
+import idatt2106v231.backend.dto.RefrigeratorDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * Class to handle Refrigerator objects
+ */
+@Service
+public class RefrigeratorServices {
+
+    /**
+     * Method to save a new refrigerator to database
+     *
+     * @param refrigeratorDto the new refrigerator
+     */
+    public void saveRefrigerator(RefrigeratorDto refrigeratorDto) {
+    }
+
+    /**
+     * Method to delete a refrigerator from database
+     *
+     * @param refrigeratorId the refrigerators id
+     */
+    public void deleteRefrigerator(int refrigeratorId) {
+    }
+
+    /**
+     * Method to get a refrigerator by id
+     *
+     * @param id the refrigerators id
+     */
+    public void getRefrigerator(int id) {
+    }
+
+    /**
+     * Method to get a refrigerator by user
+     *
+     * @param user the user
+     */
+    public void getRefrigeratorByUser(String user) {
+    }
+
+    /**
+     * Method to get all refrigerators
+     *
+     */
+    public void getAllRefrigerators() {
+    }
+
+    /**
+     * Method to get all items in refrigerator
+     *
+     */
+    public void getItemsInRefrigerator(int id) {
+    }
+}
