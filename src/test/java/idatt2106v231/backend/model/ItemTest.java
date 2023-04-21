fa3da63ba154c8cebe59dd1832ec29f8cecefeac
Anna Tvedt <amtvedt@stud.ntnu.no>
2023-04-21 14:21:04 +0200

    Implemented model tests for Category, Item and Refrigerator

---
+++ src/test/java/idatt2106v231/backend/model/ItemTest.java
@@ -0,0 +1,28 @@
+package idatt2106v231.backend.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setName() {
+    }
+}
