f3482c215b2f597698d31b221fde58664bab5156
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-04-24 16:42:16 +0200

    Added endpoint for updating a user, with test

--- src/main/java/idatt2106v231/backend/controller/UserController.java
+++ src/main/java/idatt2106v231/backend/controller/UserController.java
@@ -2,7 +2,13 @@
 package idatt2106v231.backend.controller;
 
 import idatt2106v231.backend.dto.user.UserCreationDto;
+import idatt2106v231.backend.dto.user.UserUpdateDto;
 import idatt2106v231.backend.service.UserServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +20,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private UserServices userServices;
+
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     public void setUserService(UserServices userServices) {
         this.userServices=userServices;
@@ -35,4 +44,26 @@ public class UserController {
         }
     }
 
+    @PutMapping("/updateUser")
+    @Operation(summary = "Update a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is updated"),
+            @ApiResponse(responseCode = "404", description = "User does not exist"),
+            @ApiResponse(responseCode = "500", description = "User could not be update")
+    })
+    public ResponseEntity<Object> updateUser(@RequestParam String email, @RequestBody UserUpdateDto userUpdateDto) {
+
+        ResponseEntity<Object> response;
+
+        if (!userServices.checkIfUserExists(email)){
+            response = new ResponseEntity<>("User does not exists", HttpStatus.NOT_FOUND);
+            logger.info(String.valueOf(response.getBody()));
+            return response;
+        } else if(userServices.updateUser(email, userUpdateDto)){
+            response = new ResponseEntity<>("User is updated", HttpStatus.OK);
+        } else{
+            response = new ResponseEntity<>("User could not be updated", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return response;
+    }
 }
