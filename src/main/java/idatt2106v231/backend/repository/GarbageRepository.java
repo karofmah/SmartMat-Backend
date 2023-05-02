ee817e7d9fa7baeb15af31254ef2dd70a519d194
Anna Tvedt <amtvedt@stud.ntnu.no>
2023-05-02 10:20:39 +0200

    [GARBAGE]: Updated year to YearMonth

--- src/main/java/idatt2106v231/backend/repository/GarbageRepository.java
+++ src/main/java/idatt2106v231/backend/repository/GarbageRepository.java
@@ -4,10 +4,14 @@ import idatt2106v231.backend.model.Garbage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.management.OperatingSystemMXBean;
+import java.time.YearMonth;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GarbageRepository extends CrudRepository<Garbage, Integer> {
     List<Garbage> findAll();
 
+    Optional<Garbage> findByRefrigeratorRefrigeratorIdAndDate(int refrigeratorId, YearMonth date);
 }
