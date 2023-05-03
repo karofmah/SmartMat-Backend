6e2a500e9a237737a4aca10d48ad9ee3bbe968f4
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-05-03 10:04:58 +0200

    [GARBAGE]: Clean up classes

--- src/main/java/idatt2106v231/backend/repository/GarbageRepository.java
+++ src/main/java/idatt2106v231/backend/repository/GarbageRepository.java
@@ -1,12 +1,9 @@
 package idatt2106v231.backend.repository;
 
 import idatt2106v231.backend.model.Garbage;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.management.OperatingSystemMXBean;
-import java.time.Year;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +14,7 @@ public interface GarbageRepository extends CrudRepository<Garbage, Integer> {
 
     Optional<Garbage> findByRefrigeratorRefrigeratorIdAndDate(int refrigeratorId, YearMonth date);
 
-    List<Garbage> findAllByRefrigeratorRefrigeratorIdAndDate(int refrigeratorId, YearMonth date);
-
-    List<Garbage> findAllByRefrigeratorRefrigeratorIdAndDateIsBetween(int refrigeratorId, YearMonth date, YearMonth date2);
-   // List<Garbage> findAllByRefrigeratorRefrigeratorIdAndDate_Year(int refrigeratorId, Year year);
+    List<Garbage> findAllByRefrigeratorRefrigeratorIdAndDateIsBetween(int refrigeratorId, YearMonth start, YearMonth end);
 
 
 }
